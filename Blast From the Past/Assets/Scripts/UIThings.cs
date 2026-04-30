75cf0087bfac4863feed2f35ef94482d43b133b6
MatthewLM1 <mlm24e@acu.edu>
2026-04-30 16:53:09 -0700

    Added the UI
    some of the stuff is a placeholder but can be easily swapped out.

--- Blast From the Past/Assets/Scripts/UIThings.cs
+++ Blast From the Past/Assets/Scripts/UIThings.cs
@@ -6,12 +6,21 @@ public class UIThings : MonoBehaviour
 {
     float timer = 90f;
     public TextMeshProUGUI timerText;
+
     int health = 5;
     public TextMeshProUGUI healthText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Matthew: So basically there is ammo and boss health variables here, when scripts for shooting with ammo and boss health are added in
+    // you can swap this stuff out with the actual stats. These are kinda just placeholders for when that stuff gets added.
+
+    public int currentAmmo = 30;
+    public TextMeshProUGUI ammoText;
+
+    public float currentBossHealth = 100f;
+    public TextMeshProUGUI bossHealthText;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,9 +29,12 @@ public class UIThings : MonoBehaviour
         timer -= Time.deltaTime;
         int minutes = Mathf.FloorToInt(timer / 60);
         int seconds = Mathf.FloorToInt(timer % 60);
-        timerText.text = "Recharged In: "+ string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = "Recharged In: " + string.Format("{0:00}:{1:00}", minutes, seconds);
 
         healthText.text = "Health: " + health;
+
+        ammoText.text = "Ammo: " + currentAmmo;
+        bossHealthText.text = "Boss Health: " + currentBossHealth;
     }
 
     public void ReduceTime(float amount)
@@ -33,5 +45,15 @@ public class UIThings : MonoBehaviour
     public void Heal()
     {
         health++;
+    }
+
+    public void UseAmmo(int amountUsed)
+    {
+        currentAmmo -= amountUsed;
+    }
+
+    public void DamageBoss(float damageTaken)
+    {
+        currentBossHealth -= damageTaken;
     }
 }
